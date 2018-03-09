e49139df05442ae320b8290d94168f7c6940d209
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-09 04:02:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
   string=""
   array.each_with_index do |element,index|
     case index
-    when array.size-1
-      string+="#{element}."
-    when array.size-2
-      string+="#{element}, and "
-    else
-      string+="#{element}, "
+      when array.size-1
+        string+="#{element}."
+      when array.size-2
+        string+="#{element}, and "
+      else
+        string+="#{element}, "
   end
 end
